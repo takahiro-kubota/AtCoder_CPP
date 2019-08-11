d71b4636e591c913be95d63ea830d405863d02b4
Takahiro <takahirok30@gmail.com>
2019-08-11 09:38:59 +0900

    init

---
+++ tplt/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <limits.h>
+#include <stack>
+#include <complex>
+#include <stdlib.h>
+#include <stdio.h>
+#include <functional>
+#include <cfloat>
+
+#define Rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define Sort(a) sort(a.begin(), a.end())
+#define Reverse(a) reverse(a.begin(), a.end())
+
+#define eb emplace_back
+#define ef emplace_front
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+#define fs first
+#define sc second
+     
+using namespace std;
+     
+     
+typedef long long ll;
+typedef pair<ll, ll> P;
+     
+     
+int main(){
+  ll N, K;
+
+  cin >> N >> K;
+  
+  cout << N << endl;
+
+  return 0;
+}
